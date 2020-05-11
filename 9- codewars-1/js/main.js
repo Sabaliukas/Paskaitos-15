fbe88810c99cba96a640a56c747a7c17181bd3d7
Sabaliukas <63238879+Sabaliukas@users.noreply.github.com>
2020-05-11 23:43:24 +0300

    Summation

---
+++ 9- codewars-1/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+var summation = function (num) {
+    // 1, 2, 3... num
+    let suma = 0;
+    for ( let i=1; i<=num; i++ ){
+        suma = suma +i;
+    }
+    return suma;
+  }
+
+  console.log( summation(1), '->', 1 );
+  console.log( summation(2), '->', 3 );
+  console.log( summation(3), '->', 6 );
+  console.log( summation(4), '->', 10 );
+  
