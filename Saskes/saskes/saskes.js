6b23dca9056a70aa584412d53b73a27a39ab7a10
Sabaliukas <63238879+Sabaliukas@users.noreply.github.com>
2020-05-11 12:28:53 +0300

    saskesJS

---
+++ Saskes/saskes/saskes.js
@@ -0,0 +1,98 @@
+"use strict"
+
+function renderSaskes ( size ) {
+    let HTML = '';
+    const DOMsaskes = document.querySelector('.board');
+    
+    // validacija
+
+    // logika
+    HTML = `
+        <div class="row" style="height: 12.5%;">
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+        </div>
+        <div class="row" style="height: 12.5%;">
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+        </div>
+        <div class="row" style="height: 12.5%;">
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+        </div>
+        <div class="row" style="height: 12.5%;">
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+        </div>   
+        <div class="row" style="height: 12.5%;">
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+        </div>
+        <div class="row" style="height: 12.5%;">
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+        </div>   
+        <div class="row" style="height: 12.5%;">
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+        </div>
+        <div class="row" style="height: 12.5%;">
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            <div class="cell white" style="width: 12.5%;"></div>
+            <div class="cell black" style="width: 12.5%;"></div>
+            </div>`;
+    ;
+    DOMsaskes.innerHTML = HTML
+
+
+    return;
+}
+
+renderSaskes( 8 );
