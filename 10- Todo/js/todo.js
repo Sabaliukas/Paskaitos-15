6681e73ea5054c8abe4a9e128cef0488df057fe1
Sabaliukas <63238879+Sabaliukas@users.noreply.github.com>
2020-05-12 03:33:57 +0300

    todo

---
+++ 10- Todo/js/todo.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/*
+TODO darba aprasancio objekto savybes:
+- description
+- created_on
+- deadline
+-status: todo, in-progres, done
+*/
+
+let todo_list = [
+    'aasd',
+    'ghgjg',
+    'sdsdfs'
+];
+
+console.log(todo_list);
